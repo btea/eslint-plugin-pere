54ce200c1438c2eed73f0172482c7e7958601df5
btea <2356281422@qq.com>
2025-08-08 14:21:12 +0800

    feat: no-startswith-one-char add fix

--- src/rules/no-startswith-one-char.ts
+++ src/rules/no-startswith-one-char.ts
@@ -12,18 +12,31 @@ const rule: Rule.RuleModule = {
 	create(context) {
 		return {
 			CallExpression(node) {
+        const isLiternal = () => node.callee.object.type === 'Literal' || node.callee.object.type === 'TemplateLiteral' || node.callee.object.type === 'Identifier'
+        const isMemberExpression = () => node.callee.object.type === 'MemberExpression'
 				if (node.callee.type === 'MemberExpression' &&
 					node.callee.property.name === 'startsWith' &&
-					(node.callee.object.type === 'Literal' || node.callee.object.type === 'TemplateLiteral' || node.callee.object.type === 'Identifier') &&
+					(isLiternal() || isMemberExpression()) &&
 					node.arguments[0].type === 'Literal' &&
 					String(node.arguments[0].value)?.length === 1) {
 						context.report({
-						node,
-						message: 'Using `startsWith` with a single character is not allowed.',
-					});
-				}
+              node,
+              message: 'Using `startsWith` with a single character is not allowed.',
+              fix: fixer => {
+                if (isLiternal() || node.callee.object.type === 'Identifier') {
+                  return fixer.replaceText(node, `${node.callee.object.raw}[0] === "${node.arguments[0].value}"`)
+                }
+                if (isMemberExpression()) {
+                  if (node.callee.object.object) {
+                    return fixer.replaceText(node, `${node.callee.object.object.name}[${node.callee.object.property.name}][0] === "${node.arguments[0].value}"`)
+                  }
+                }
+                return null
+              }
+            })
+					}
 			}
-		};
+		}
 	}
 }
 
