b6b00325d251665cb6337e09044372df44d304a7
btea <2356281422@qq.com>
2025-08-11 18:01:45 +0800

    feat: import types/etree & update rule

--- src/rules/duplicate-constructor.ts
+++ src/rules/duplicate-constructor.ts
@@ -1,4 +1,5 @@
 import type { Rule } from "eslint";
+import type * as ESTree from "estree"
 
 // for (let i = 0; i < 10; i++) {
 //  1、const a = /a/
@@ -17,37 +18,77 @@ const rule: Rule.RuleModule = {
 		schema: [],
 	},
 	create(context) {
-    const isLiteral = (node: Rule.Node) => {
-      return node.type === 'Literal';
+    const isLiteral = (node: Rule.Node | ESTree.Expression) => {
+      return node.type === 'Literal'
+    }
+    const isPropertyLiteral = (property: ESTree.Property): boolean => {
+      return property.key?.type === 'Literal' && property.value?.type === 'Literal'
     }
     const isInLoop = (node: Rule.Node): boolean => {
       if (!node.parent) {
-        return false;
+        return false
       }
       if (['ForStatement', 'WhileStatement', 'DoWhileStatement', 'ForInStatement', 'ForOfStatement'].includes(node.parent.type)) {
-        return true;
+        return true
       }
-      return isInLoop(node.parent);
+      return isInLoop(node.parent)
     }
+    const isArrayExpression = (type: string): boolean => {
+      return type === 'ArrayExpression'
+    }
+    const isPureArray = (node: ESTree.ArrayExpression): boolean => {
+      return isArrayExpression(node.type) && node.elements.length > 0 && node.elements.every((e) => isLiteral(e as ESTree.Expression))
+    }
+    const isObjectExpression = (type: string): boolean => {
+      return type === 'ObjectExpression'
+    }
+    const isPureObject = (node: ESTree.ObjectExpression): boolean => {
+      return isObjectExpression(node.type) && node.properties.every((p) => isPropertyLiteral(p as ESTree.Property))
+    }
+
+    const report = (node: ESTree.Node, message: string) => {
+      context.report({
+        node,
+        message,
+      });
+    };
+
 		return {
       CallExpression(node) {
-        if (node.callee.type === 'NewExpression' && node.callee.callee.type === 'Identifier') {
+        if (node.callee.type === 'NewExpression') {
           const args = node.arguments;
           if (args.length === 0) {
             return; // Skip if no arguments are provided
           }
-          if (args.every(() => {
-            // @ts-expect-error
-            return isLiteral(args[0]);
+          if (args.every((arg) => {
+            return isLiteral(arg as ESTree.Expression);
           }) && isInLoop(node)) {
-            const className = node.callee.callee.name;
-            context.report({
-              node,
-              message: `Repetitive construction of '${className}' detected.`,
-            });
+            report(node, `Repetitive construction of '${node.callee.type}' detected.`);
+          }
+        } else if (node.callee.type === 'MemberExpression' && node.callee.object.type === 'ArrayExpression') {
+          const array = node.callee.object;
+          if (isPureArray(array) && isInLoop(node)) {
+            report(node, `Repetitive construction of 'Array' detected.`);
+          }
+        } else if (node.callee.type === 'MemberExpression' && node.callee.object.type === 'ObjectExpression') {
+          const object = node.callee.object;
+          if (isPureObject(object) && isInLoop(node)) {
+            report(node, `Repetitive construction of 'Object' detected.`)
           }
         }
       },
+      VariableDeclarator(node) {
+        if (node.init) {
+          if (isArrayExpression(node.init.type) && isPureArray(node.init as ESTree.ArrayExpression) && isInLoop(node)) {
+            report(node, `Repetitive construction of 'Array' detected.`);
+            return
+          }
+          if (isObjectExpression(node.init.type) && isPureObject(node.init as ESTree.ObjectExpression) && isInLoop(node)) {
+            report(node, `Repetitive construction of 'Object' detected.`);
+            return
+          }
+        }
+      }
     };
 	}
 }
--- src/rules/no-index-of-assert-zero.ts
+++ src/rules/no-index-of-assert-zero.ts
@@ -1,4 +1,5 @@
 import type { Rule } from "eslint"
+import type * as ESTree from "estree"
 
 const rule: Rule.RuleModule = {
   meta: {
@@ -8,21 +9,30 @@ const rule: Rule.RuleModule = {
       recommended: true,
     },
     schema: [],
+    fixable: 'code',
   },
   create(context) {
     return {
-      BinaryExpression(node) {
+      BinaryExpression(node: ESTree.BinaryExpression) {
         if (node.operator === '===' &&
-            node.left.type === 'CallExpression' &&
-            node.left.callee.type === 'MemberExpression' &&
-            node.left.callee.property.type === 'Identifier' &&
-            node.left.callee.property.name === 'indexOf' &&
-            node.right.type === 'Literal' &&
-            node.right.value === 0) {
-          context.report({
-            node,
-            message: 'Using `indexOf` to find the first element is not allowed.',
-          });
+          node.left.type === 'CallExpression' &&
+          node.left.callee.type === 'MemberExpression' &&
+          node.left.callee.property.type === 'Identifier' &&
+          node.left.callee.property.name === 'indexOf' &&
+          node.right.type === 'Literal' &&
+          node.right.value === 0) {
+              const left = node.left as ESTree.CallExpression;
+              const right = node.right as ESTree.Literal;
+              const leftCallee = left.callee as ESTree.MemberExpression;
+            context.report({
+              node,
+              message: 'Using `indexOf` to find the first element is not allowed. Use `===` instead.',
+              fix: fixer => {
+                const sourceCode = context.sourceCode ?? context.getSourceCode();
+                const objectText = sourceCode.getText(leftCallee.object);
+                return fixer.replaceText(node, `${objectText}[0] === ${right.raw}`);
+              }
+            });
         }
       }
     };
